1985c92eb51004474475a479660c006956232dc4
RamonaJudithTorres <ramonablacktorres@gmail.com>
2022-10-11 09:33:53 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react';
+import store from './store';
+import { Provider } from 'react-redux';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // 2. Wrap ChakraProvider at the root of your app
+  <ChakraProvider>
+    <React.StrictMode>
+      <Provider store={store}>
+      <App />
+      </Provider>
+   </React.StrictMode>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
